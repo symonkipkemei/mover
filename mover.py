161cd7686020d9b8b00aab08dfeb21677d865ce2
symonkipkemei <symonkipkemei@gmail.com>
2023-04-02 13:16:58 +0300

    goes upto level 2 and move files from one directory to another

--- mover.py
+++ mover.py
@@ -2,32 +2,61 @@ import pathlib
 
 
 # path to my desktop
-path = pathlib.Path("/home/symon_kipkemei")
+start_path = "/mnt/d/New folder"
+start_path_obj = pathlib.Path(start_path)
+
 
 
 # create new folder
-new_path = pathlib.Path("/home/symon_kipkemei/shots")
+new_path = pathlib.Path(f"{start_path}/Revit-bastards")
 new_path.mkdir(exist_ok=True )
 
 
+def check_move_files(path,new_path) -> None:
+    """check if file is  png it then moves to a separate folder
 
-# create an object that points to where the images are 
+    Args:
+        path (Path): path of the current folder/file/directory
 
-for images in path.iterdir():
-    print(images.name)
-    if images.suffix == ".png":
-         # create a new path for the images 
-        new_filepath = new_path.joinpath(images.name)
+    """
+
+    if path.suffix == ".rvt" or path.suffix == ".txt":
+        # create a new path for the level_1 
+       new_filepath = new_path.joinpath(path.name)
     
-        #move files
-        images.replace(new_filepath)
+       #move files
+       path.replace(new_filepath)
 
-    elif images.suffix == ".PNG":
-         # create a new path for the images 
-        new_filepath = new_path.joinpath(images.name)
-    
-        #move files
-        images.replace(new_filepath)
 
+
+
+# create an object that points to where the level_1 are 
+
+
+# starting level
+for level_1 in start_path_obj.iterdir():
+    if level_1.is_file():
+        check_move_files(level_1,new_path)
+
+    # level 1
+    else:
+        level_1_path = start_path_obj.joinpath(level_1)
+        for level_2 in level_1_path.iterdir():
+            if level_2.is_file():
+                check_move_files(level_2,new_path)
+            #level 2
+            else:
+                level_2_path = level_1_path.joinpath(level_2)
+                for level_3 in level_2_path.iterdir():
+                    if level_2.is_file():
+                        check_move_files(level_3,new_path)
+
+                
+        
+        
+
+
+
+        
 
 print("completed succesfully")
